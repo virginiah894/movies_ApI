6394b0b5581af8f10f6e0444810298041dfe5ed7
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-08-19 18:38:26 +0300

    made changes to the app by using a new api to get the list of the latest movies. added some styling to the app

--- src/App.js
+++ src/App.js
@@ -1,71 +1,17 @@
-import './App.css';
-import SearchIcon from './search.svg';
-import MovieCard from './components/moviecard';
+import Movies from "./components/movies/movies.component"
 
-import { useEffect ,useState} from 'react';
+const App = () => {
 
-const API_URL= 'http://www.omdbapi.com?apikey=13d25c74';
-const movie1 = {
-  Poster: "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
-  Title: "Italian Spiderman",
-  Type: "movie",
-  Year: "2007",
-  imdbID: "tt2705436"
-}
-const App=()=> {
-  const [movies, setMovies]= useState([])
-  const searchMovies =async(title)=> {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-    setMovies(data.Search);
-  }
-  useEffect(()=>{
-    searchMovies('spiderman');
-    
+return(
 
-  }, []);
-  return(
-    <div className="App">
-    
-       <h1>Movies haven</h1>
-       <div className="search">
-        <input
-        placehlder = "search for movies"
-        value="superman"
-        onChange={()=>{}}/>
-        <img
-        src="{searchIcon}"
-        alt ="Search"
-        onClick={()=>{}}
-        />
-       </div>
-       {movies?.length>0?
-       (
-       <div className='container'>
-        {movies.map((movie)=>(
-        <MovieCard movie={movie}/>))}
-        
-       </div>
-       ):
-       (
-        <div className='empty'>
-          <h2>No movies found</h2>
-        </div>
-       )
-       }
+  <div>
+  <h1 style={{textAlign:'center', marginTop:'2rem'}}> Latest Movies </h1>
+ 
+    <Movies/>
+ 
+  </div>
+)
 
-      
-      
-      
-  
-    </div>
-
-  )
-    
-
-  
-  
 }
 
-
-export default App;
+export default App
